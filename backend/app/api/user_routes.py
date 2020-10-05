94d087621b929204b95789a6d1c49b401e1af837
Brandon Held <wildwestcowboy12@gmail.com>
2020-10-05 15:59:16 -0500

    first commit

---
+++ backend/app/api/user_routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, jsonify
+from app.models import User
+from app.models import User, db
+from flask_login import current_user
+
+user_routes = Blueprint('users', __name__)
+
+
+@user_routes.route('/')
+def index():
+    response = User.query.all()
+    return {"users": [user.to_dict() for user in response]}
+
+
+@user_routes.route('/signup', methods=['POST'])
+def signup_user():
+    user = User(
+                username=request.json.get('username', None),
+                email=request.json.get('email', None),
+                password=request.json.get('password', None)
+    )
+    db.session.add(user)
+    db.session.commit()
+    email = user.email
+    session["user"] = user.to_dict()
+    return {"user": user.to_dict()}, 200
---
+++ client/README.md
@@ -0,0 +1,9 @@
+# Running The Frontend in Development
+1. Run `npm install`.
+2. Install other dependencies
+3. Run this application using `npm start`.
+
+This app will be automatically built when you deploy the container to heroku. Please see the README in the root of your project skeleton for information about deployment.
+
+### Navigation
+* [Back to root README](../README.md)
