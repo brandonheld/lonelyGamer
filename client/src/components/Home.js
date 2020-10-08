229a183aa25f37183fda4e66d2ca4cf92fd618ef
Brandon Held <wildwestcowboy12@gmail.com>
2020-10-07 19:10:24 -0500

    being of homepage and auth 100% done

---
+++ client/src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom'
+import { logout } from '../store/auth';
+
+import '../css/home.css'
+
+function Home() {
+    const dispatch = useDispatch();
+
+    const currentUserId = useSelector(state => state.auth.id);
+
+    const signOut = e => {
+        e.preventDefault();
+        dispatch(logout());
+    }
+
+    if (!currentUserId) return <Redirect to='/' />
+
+    return (
+        <div className='homeContainer'>
+            <div className='homeContainer__left'>
+
+            </div>
+            <div className='homeContainer__right'>
+                <button id='logout' onClick={signOut}>Sign Out</button>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Home;
--- client/src/components/Pages.js
+++ client/src/components/Pages.js
@@ -3,10 +3,12 @@ import { Route } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
 import Splash from './Splash'
+import Home from './Home'
 
 function Pages() {
     return (
         <>
+            <Route exact path='/home' component={Home} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/signup' component={Signup} />
             <Route exact path='/' component={Splash} />
