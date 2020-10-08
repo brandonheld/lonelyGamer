77ee9d57a5e3a0bd81d203fb0731315cbc56952d
Brandon Held <wildwestcowboy12@gmail.com>
2020-10-08 16:10:14 -0500

    changed css

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, useHistory } from 'react-router-dom';
 import { login } from '../store/auth';
@@ -21,10 +21,39 @@ function Login() {
         e.preventDefault();
         history.push(`/signup`)
     }
+    
+    const createSquare = () => {
+        const section = document.querySelector('section');
+        const square = document.createElement('span');
+        
+        let size = Math.random() * 50;
+        
+        square.style.width = 20 + size + 'px';
+        square.style.height = 20 + size + 'px';
+        square.style.top = Math.random() * window.innerHeight + 'px';
+        square.style.left = Math.random() * window.innerWidth + 'px';
+        
+        
+        section.appendChild(square);
+
+        setTimeout(() => {
+            square.remove()
+        }, 5000)
+    }
+    useEffect(() => {
+        const interval = setInterval(createSquare, 150);
+
+        return () => {
+            clearInterval(interval);
+        }
+
+    }, [])
+
     if (currentUserId) return <Redirect to='/home' />
 
     return (
         <div className='loginBackground'>
+            <section id='boxes'></section>
             <div className='loginContainer'>
                 <div className='loginContainer__header'>
                     <h1>Login</h1>
